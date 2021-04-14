6c2e9de0683578f4eec18f44d7ca1ec0a6878b1d
Gabriel Augustinho <gabriel.aug.alves@outlook.com>
2021-04-14 11:57:07 -0300

    Criei um programa básico mostrando a mensagem ola mundo

---
+++ programas-testes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programas_testes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Olá mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
